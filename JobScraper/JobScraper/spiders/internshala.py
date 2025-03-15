affca30cb5b3acf851d11639e6ae07b3c5b2dcff
Nishu Kumari <nishu.240103211@iiitbh.ac.in>
2025-03-16 00:03:51 +0530

    fixing issue

--- JobScraper/JobScraper/spiders/internshala.py
+++ JobScraper/JobScraper/spiders/internshala.py
@@ -1,1 +1,24 @@
-print("coding for internshala.............................")
+
+import scrapy
+from properties.p import Property
+
+
+class InternshalaSpider(scrapy.Spider):
+    name = "internshala"
+    prop = Property()
+    p = prop.load_property('properties/internshala.properties')
+    url1 = p.get('START_URL1')
+    url2 = p.get('START_URL2')
+    print("url =",url1)
+
+
+    start_urls = [
+        url1,
+        url2
+    ]
+
+
+
+    def parse(self, response):
+
+        print("here.")
